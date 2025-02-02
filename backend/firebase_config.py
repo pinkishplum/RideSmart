07f92433ec9b7b597d24d3f59a7c85d3d8af0e92
ZIYAD EMAD MOWAFY <122952696+pinkishplum@users.noreply.github.com>
2025-02-02 09:38:19 +0300

    fff

--- backend/firebase_config.py
+++ backend/firebase_config.py
@@ -1,10 +1,15 @@
 import os
-import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+# Get the absolute directory of this file
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-cred = credentials.Certificate("ridesmart-9fa62-firebase-adminsdk-fbsvc-63a6f51c4d.json")
+# Build the absolute path to the service account key file
+cert_path = os.path.join(basedir, "ridesmart-9fa62-firebase-adminsdk-fbsvc-63a6f51c4d.json")
+
+# Use the absolute path to initialize the credentials
+cred = credentials.Certificate(cert_path)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
